8a2569090918665a6049e80fa2a33c1f0d450bd8
Mehmet Ugur SATIR <musatir@gmail.com>
2022-05-21 20:02:54 +0300

    CheckoutForm Project is uploaded

---
+++ app.js
@@ -0,0 +1,12 @@
+function mailValidator(){
+    const mail = document.getElementById("email").value;
+
+    const pattern1 = /[.][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]$/;
+    const pattern2 = /[.][a-zA-Z0-9][a-zA-Z0-9]$/;
+
+    if ((pattern1.test(mail) || pattern2.test(mail)) && (mail.indexOf('@', 1) != -1) && (mail.indexOf('@') == mail.lastIndexOf('@')) && (!/[@][.]/.test(mail)) && (!mail.includes(' '))) {
+        alert("Your e-mail address is valid!");
+    } else {
+        alert("Your e-mail address is not valid!");
+    }
+}
